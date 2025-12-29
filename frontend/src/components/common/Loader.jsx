93be98714fffab4f1fb569a12b5daf6dd1e55eb8
ankitsingh711 <developerankit2127@gmail.com>
2025-12-29 18:03:55 +0530

    styler-frontend

---
+++ frontend/src/components/common/Loader.jsx
@@ -0,0 +1,12 @@
+import { FaCut } from 'react-icons/fa';
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <FaCut className="loader-icon" />
+        </div>
+    );
+};
+
+export default Loader;
